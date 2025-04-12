641023133f2e3de493b7a308025490c5076e6565
Alex Uta <marian.alexandru.uta@gmail.com>
2025-04-12 23:35:12 +0100

    FLRC

--- code/RC-Receiver_Car/example.user_config.hpp
+++ code/RC-Receiver_Car/example.user_config.hpp
@@ -35,15 +35,13 @@
 */
 #define RADIOLIB_SHAPING_1_0                                    (0x04)
 
-float   SX1280_FREQUENCY                    = 2410; // Remote TX
-int     SX1280_FREQUENCY_DEVIATION          = 200;
-int     SX1280_BIT_RATE                     = 250;
-int     SX1280_OUTPUT_POWER                 = 8; // -18 to 13 dBm
-int     SX1280_GAIN_CONTROL                 = 8; // 1 - 13
-uint8_t SX1280_DATA_SHAPING                 = RADIOLIB_SHAPING_0_5;
-int     SX1280_CRC_VALUE                    = 1;
-int     SX1280_PREAMBLE_LENGTH              = 4;
-bool    SX1280_SET_HIGH_SENSITIVITY_MODE    = true;
+float   SX1280_FREQUENCY            = 2410;
+int     SX1280_BIT_RATE             = 260; // 260, 325, 520, 650, 1000 and 1300
+int     SX1280_CODING_RATE          = 2;
+int     SX1280_OUTPUT_POWER         = 8; // -18 to 13 dBm
+int     SX1280_GAIN_CONTROL         = 8; // 1 - 13
+uint8_t SX1280_DATA_SHAPING         = RADIOLIB_SHAPING_0_5;
+uint8_t SX1280_SYNC_WORD[]          = {0x01, 0x23, 0x45, 0x67};
 
 /**
  * Servo & DC Motor
